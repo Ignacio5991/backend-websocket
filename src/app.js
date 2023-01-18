76c38072883d1428285035bacdf6c9b7af8ba92e
Ignacio Gonzalez Sorribas <igonzalezsorribas@gmail.com>
2023-01-18 16:59:03 -0300

    Creando el primer repositorio remoto

---
+++ src/app.js
@@ -0,0 +1,39 @@
+const express = require ('express');
+const server = express();
+const productsroute = require ('./routes/products.route')
+const cartsroute = require ('./routes/cart.route')
+server.use(express.urlencoded({extended:true}))
+server.use(express.json())
+
+server.use('/api/products/', productsroute);
+server.use('/api/products/',productsroute);
+server.use('/api/products/',productsroute);
+server.use('/api/products/',productsroute);
+server.use('/api/products/',productsroute);
+
+
+//Rutas del cart
+
+server.use('/api/carts/',cartsroute);
+server.use('/api/carts/',cartsroute);
+server.use('/api/carts/',cartsroute);
+
+server.listen(8080,()=>{
+    console.log("Servidor Escuchando en el puerto 8080")
+})
+
+//handlebar
+app.engine ('handlebars', handlebars.engine());
+
+app.set('view.engine','handlebars');
+
+app.use(express.static, ('public'));
+
+app.use('/',router.Views);
+
+//socket
+const socketServer =socketIo(httpServer);
+
+socketServer.on('conection',(socket)=>{
+    console.log('cliente conectado')
+})
---
+++ src/controller/cart.controller.js
@@ -0,0 +1,48 @@
+const cartManager = require('../cartmanager');
+const ProductManager = require('../productmanager');
+
+const cart = new cartManager(__dirname + '/../cart.json');
+const pm = new ProductManager('./src/products.json');
+
+const createcart = async (req, res) => {
+  const resp = await cart.CreateFile();
+  res.json({ msg: 'se creo el carro de forma exitosa', id: resp });
+};
+
+const getchango = async (req, res) => {
+  const resp = await cart.getchango(req.params.cid);
+  if (resp.error) {
+    res.json(resp.status).send(resp);
+  } else {
+    res.json(resp);
+  }
+};
+
+const addProductCart = async (req, res) => {
+  const { cid, pid } = req.params;
+  const products = await pm.getProductsById(pid);
+  
+  if (products) {
+      const resp = await cart.addProductCart(cid, products.id);
+      res.json  ({ msg:"Producto Agregado"})  
+  }else {
+      res.json  ({ msg:"Producto no Encontrado"})  
+  }
+  
+};
+//   if (products.error) {
+//     res.json(products.status).send(products);
+//   } else {
+//     const respu = await cart.addProductCart(cid, products.id);
+//     if (respu.error) {
+//       res.json(respu.status).send(respu);
+//     } else {
+//       res.json({ msg: 'Producto agregado con exito' });
+//     }
+//   }
+
+module.exports = {
+  createcart,
+  getchango,
+  addProductCart
+};
