a55f040633056001e2c42b2fc9b463a2316c2245
Ignacio Gonzalez Sorribas <igonzalezsorribas@gmail.com>
2023-01-19 19:00:22 -0300

    Tratando de levantar el servidor

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require ('express');
-const {Server} = requiere ('socket.io');
+const {Server} = require ('socket.io');
 const app = express();
 const handlebars = require ('express-handlebars');
 const routerViews = require ('./routes/views.route')
@@ -15,41 +15,38 @@ io.on('conection',socket =>{
 
 
 
-const server = express();
-const productsroute = require ('./routes/products.route')
-const cartsroute = require ('./routes/cart.route')
-server.use(express.urlencoded({extended:true}))
-server.use(express.json())
+// const server = express();
+// const productsroute = require ('./routes/products.route')
+// const cartsroute = require ('./routes/cart.route')
+// server.use(express.urlencoded({extended:true}))
+// server.use(express.json())
 
-server.use('/api/products/', productsroute);
-server.use('/api/products/',productsroute);
-server.use('/api/products/',productsroute);
-server.use('/api/products/',productsroute);
-server.use('/api/products/',productsroute);
+// server.use('/api/products/', productsroute);
+// server.use('/api/products/',productsroute);
+// server.use('/api/products/',productsroute);
+// server.use('/api/products/',productsroute);
+// server.use('/api/products/',productsroute);
 
 
-//Rutas del cart
+// //Rutas del cart
 
-server.use('/api/carts/',cartsroute);
-server.use('/api/carts/',cartsroute);
-server.use('/api/carts/',cartsroute);
+// server.use('/api/carts/',cartsroute);
+// server.use('/api/carts/',cartsroute);
+// server.use('/api/carts/',cartsroute);
 
-server.listen(8080,()=>{
-    console.log("Servidor Escuchando en el puerto 8080")
-})
+// server.listen(8080,()=>{
+//     console.log("Servidor Escuchando en el puerto 8080")
+// })
 
 //handlebar
 app.engine ('handlebars', handlebars.engine());
 
-app.set('view.engine','handlebars');
+app.set('views',__dirname +'/views');
+app.set('view engine', 'handlebars');
 
-app.use(express.static, ('public'));
+app.use(express.static, (__dirname +'/public'));
 
 app.use('/',router.Views);
 
-//socket
-const socketServer =socketIo(httpServer);
 
-socketServer.on('conection',(socket)=>{
-    console.log('cliente conectado')
-})
+
