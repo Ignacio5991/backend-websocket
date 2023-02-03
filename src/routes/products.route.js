f3f573f6dc7ba3a1162826a8c0dc8c7765e14c2d
Ignacio Gonzalez Sorribas <igonzalezsorribas@gmail.com>
2023-02-03 17:28:36 -0300

    Crando la ruta de prodcuts route para mongoose

--- src/routes/products.route.js
+++ src/routes/products.route.js
@@ -1,6 +1,7 @@
 const {Router} = require ("express")
 
 const productscontroller = require ('../controller/products.controller');
+const ProductsModel = require("../models/products.model");
 
 const router = Router (); 
 
@@ -9,6 +10,24 @@ router.get('/:pid',productscontroller.ProductsbyId);
 router.post('/',productscontroller.addProduct);
 router.put('/:pid',productscontroller.updateProducts);
 router.delete('/:pid',productscontroller.deleteProduct);
+
+
+/*Rutas de Mongoose*/ 
+
+router.get('/',async (req,res)=>{
+    try{
+    const products = await ProductsModel.find();
+    return res.status(200).json({
+        msg:'Ok',
+        payload:'products'
+    })
+    }catch (error){
+        return res.status(500).json({
+            msg:'error',
+            payload:'error al obtener los productos'
+        })
+    }
+})
 
 
 module.exports = router;
